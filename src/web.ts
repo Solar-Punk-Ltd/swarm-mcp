aa57264347c6e1f4ff5b49ed9ac0ae27b2466b2b
AndreiMitrea <andrei.mitrea.hq@gmail.com>
2026-03-10 08:33:25 +0200

    chore: Removed SSE option that was marked deprecated (#29)
    * chore: Removed SSE option that was marked deprecated
    
    * fix: Added logging capability to fix MCP client error
    
    * fix: Fixed schema description text
    
    * fix: Removed SSE from README
    
    * fix: Updated README

--- src/web.ts
+++ src/web.ts
@@ -4,7 +4,6 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { SwarmMCPServer } from "./mcp-service";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const host = process.env.HOST || "0.0.0.0";
@@ -14,12 +13,6 @@ const app = express();
 // Enable CORS for all routes
 app.use(cors());
 app.use(express.json());
-
-// For storing SSE transports
-const transports: Map<string, SSEServerTransport> = new Map<
-  string,
-  SSEServerTransport
->();
 
 async function main() {
   // Setup for stateless HTTP transport
@@ -44,49 +37,6 @@ async function main() {
       }
     }
   });
-
-  // Setup for stateful SSE transport
-  app.get("/sse", async (req, res) => {
-    if (req.query.sessionId) {
-      res
-        .status(400)
-        .send(
-          "Reconnecting with a session ID is not supported on this endpoint."
-        );
-      return;
-    }
-
-    const { server } = new SwarmMCPServer();
-    // Create and store transport for new session
-    const transport = new SSEServerTransport("/message", res);
-    transports.set(transport.sessionId, transport);
-
-    // Connect server to transport
-    await server.connect(transport);
-
-    // Handle close of connection
-    res.on("close", () => {
-      transports.delete(transport.sessionId);
-      server.close();
-    });
-  });
-
-  app.post("/message", async (req, res) => {
-    const sessionId = req.query.sessionId as string;
-    if (!sessionId) {
-      res.status(400).send("Session ID is required");
-      return;
-    }
-
-    const transport = transports.get(sessionId);
-    if (transport) {
-      await transport.handlePostMessage(req, res, req.body);
-    } else {
-      console.error(`No transport found for sessionId ${sessionId}`);
-      res.status(403).send("Session not found");
-    }
-  });
-
   // Start the server
   app.listen(port, host, () => {});
 }
